0d1b01cfa73c4ff103700bdfa94ea540ec7dd704
4yjo <27517870+4yjo@users.noreply.github.com>
2021-06-06 14:28:51 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, //delete Debug banner
+      title: 'ciaoKater',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.amber,
+      ),
+      home: MyHomeScreen(title: 'Cheers!'),
+    );
+  }
+}
+
+class MyHomeScreen extends StatefulWidget {
+  MyHomeScreen({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomeScreenState createState() => _MyHomeScreenState();
+}
+
+class _MyHomeScreenState extends State<MyHomeScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _allDrinks = new List.generate(_counter, (int i) => new Text('Drink'));
+    //TODO diffrentiate between drinks
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        shape: ContinuousRectangleBorder(
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(30.0),
+            bottomRight: Radius.circular(30.0),
+          ),),
+      ),
+
+
+      body: new LayoutBuilder(builder: (context, constraint){
+        final _maxHeigt = constraint.biggest.height/3;
+        final _biggerFont = TextStyle(fontSize: _maxHeigt/6);
+        return new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Container(
+                height: 200.0,
+                child: new ListView(
+                  children: _allDrinks,
+                  scrollDirection: Axis.vertical,
+                ),
+              ),
+                new TextButton(
+                  onPressed: _addNewDrink,
+                    child: new Text('+Beer'),
+                ),
+                new TextButton(
+                onPressed: _addNewDrink,
+                  child: new Text('+Wine'),
+                ),
+                new TextButton(
+                onPressed: _addNewDrink,
+                  child: new Text('+Shot'),
+                ),
+            ],
+          ),
+        );
+      },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        //onPressed: _incrementCounter,
+        onPressed: _addNewDrink,
+        tooltip: 'Increment',
+        child: Text('Beer'), //Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+
+  }
+
+  void _addNewDrink() {
+    setState((){
+      _counter ++;
+    });
+  }
+}
